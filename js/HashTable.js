30dab0c2e830573d6d96db72f035ba1ca4004967
Aidar Haiatov <aidar.haiatov@gmail.com>
2019-10-23 15:07:12 +0300

    add collision functional

--- js/HashTable.js
+++ js/HashTable.js
@@ -8,19 +8,50 @@ function HashTable(size = 997) {
         for(let i = 0; i < string.length; i++){
             index += string[i].charCodeAt();
         }
-        return index;
+        return index % _size;
+    }
+
+    function foundedKeyIndex(key, list){
+        // this function is needed for search key in list (collisions)
+        // this function also stupid, only learn sample
+       for(let i = 0; i < list.length; i++){
+           if(list[i][0] == key) return i;
+       } 
+       return -1;
     }
 
     return {
 
         set(key, value) {
             const index = hash(key);
-            _data[index] = value;
+            let list = _data[index];
+
+            if(!list){
+                // list is not exist - init list, add value
+                _data[index] = [[key, value]];
+            }else{
+                const listIndex = foundedKeyIndex(key, list);
+                if(listIndex != -1){
+                    // list exist, key exist - update value
+                    list[listIndex][1] = value;
+                }else{
+                    // list exist, key don't exist - add value
+                    list.push([key, value]);
+                }
+            }
+            
         },
 
-        get() {
+        get(key) {
             const index = hash(key);
-            return _data[index];
+            let list = _data[index];
+
+            if(list){
+                const listIndex = foundedKeyIndex(key, list);
+                if(listIndex != -1){
+                    return list[listIndex][1];
+                }
+            }
         },
 
         dump(){
